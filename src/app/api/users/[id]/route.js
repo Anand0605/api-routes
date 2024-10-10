7613e3ec84438b5ff9f50b607e6bb3b86880d936
Anand Kumar Gautam <anandvns0510@gmail.com>
2024-10-11 01:45:21 +0530

    complete crud

--- src/app/api/users/[id]/route.js
+++ src/app/api/users/[id]/route.js
@@ -16,3 +16,26 @@ export const GET = (request,content)=>{
     const userData = user.filter((item)=>item.id == content.params.id) 
     return NextResponse.json(userData.length == 0 ? {result:"no data found", success: false}: {result:userData[0],success:true},{status:200})
 }
+
+export const PUT = async(request,content)=>{
+    
+    let payload = await request.json();
+    payload.id = content.params.id;
+    // let userId = content.params.id;
+    console.log(payload)
+    // console.log(userId)
+    if(!payload.id || !payload.age || !payload.email){
+        return NextResponse.json({result:"data is not valid", success:false},{status:400})
+    }
+    return NextResponse.json({result:payload, success:true},{status:200})
+
+}
+
+export const DELETE =(request,content)=>{
+let id = content.params.id
+if(id){
+    return NextResponse.json({result:"User Delete", success:true},{status:200})
+}else{
+    return NextResponse.json({result:"Internal error, please try after sometime",success:flase},{status:400})
+}
+}
